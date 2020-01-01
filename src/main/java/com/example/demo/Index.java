7e1025ec6c4be3ec99e5a621584c1c22c7e26ac7
Seldoon <1129006318@qq.com>
2020-01-01 19:32:41 +0800

    增加视图传递参数

--- src/main/java/com/example/demo/Index.java
+++ src/main/java/com/example/demo/Index.java
@@ -1,16 +1,24 @@
 package com.example.demo;
 
+import com.example.demo.model.TestModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Map;
+
 @Controller
 public class Index {
 
     @GetMapping("/index")
-    public String index() {
+    public String index(Model view, String name, String mobile) {
+        TestModel model = new TestModel();
+        model.setEmail(mobile);
+        model.setName(name);
+        view.addAttribute("test", model);
         return "Index/index";
     }
 
---
+++ src/main/java/com/example/demo/model/TestModel.java
@@ -0,0 +1,32 @@
+package com.example.demo.model;
+
+public class TestModel {
+    public String name;
+    public String email;
+
+    public int mobile;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(int mobile) {
+        this.mobile = mobile;
+    }
+}
